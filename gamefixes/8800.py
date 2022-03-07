51bc4550a3bf6c451b29dd4b888bdc2c30a30546
John Obscurant <usul@gmx.li>
2022-03-07 16:29:47 +0100

    Add protonfix for Sid Meier's Civilization IV: Beyond the Sword

---
+++ gamefixes/8800.py
@@ -0,0 +1,12 @@
+""" Sid Meier's Civilization IV: Beyond the Sword
+"""
+
+# Note: as of 2022-mar-07 buttons glitchy unless using PROTON_USE_WINED3D=1 environment
+
+from protonfixes import util
+
+def main():
+
+    util.protontricks('msxml3')
+    util.protontricks('corefonts')
+
